5e138972c972b01940501eb9f986a1c9edb84c89
Cesar Ortega <cesarluis4056@gmail.com>
2023-10-31 10:57:54 -0500

    Agregando archivo Arrays.js

---
+++ js/Arrays.js
@@ -0,0 +1,13 @@
+const arr = ["1", "2", "3", "4", "5"];
+
+// Agregando "9" con push()
+arr.push("9");
+console.log(arr);
+
+// Agregando "-8" con unshift()
+arr.unshift("-8");
+console.log(arr);
+
+// Eliminando "9" con pop();
+arr.pop();
+console.log(arr);
